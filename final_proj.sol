935f7559e7ad1eaca7297a032dff764c68659ba3
infoparth <parthatharv2606@gmail.com>
2023-06-30 12:42:35 +0530

    adding some comments

--- final_proj.sol
+++ final_proj.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; 
  
 contract redeem is Ownable{ 
-    struct  item { 
+    struct  item {   // struct to store the items along with their index and value
  
         uint  id; 
         string  name; 
@@ -13,14 +13,14 @@ contract redeem is Ownable{
          
     } 
  
-    item[] public items; 
+    item[] public items;   //struct array to store the items, that are listed
  
     uint index = 0; 
  
     function addItem(string memory _name, uint _val) public onlyOwner{ 
  
         item memory newItem = item(index, _name, _val); 
-        items.push(newItem); 
+        items.push(newItem);  //adding the items into the struct array for listing
         index += 1; 
  
     } 
@@ -31,12 +31,12 @@ contract DegenToken is ERC20, Ownable, redeem {
  
      
  
-    mapping (address => string[]) redTokens; 
+    mapping (address => string[]) redTokens;  //mapping to store the redeemed tokens to a wallet address
  
     constructor() ERC20("Degen", "DGN") {} 
  
         function mint(address to, uint256 amount) public onlyOwner { 
-            _mint(to, amount); 
+            _mint(to, amount);   //minting tokens to a particular amount
     } 
  
     function transfer_token(address _add, uint _val) public returns(bool){ 
